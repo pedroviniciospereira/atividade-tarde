e28c4dbf5b8242ed096b3114d4faa268366b1504
pedroviniciospereira <carlosviniciospranada@gmail.com>
2025-10-04 17:52:50 +0000

    feacture: migrate DB

--- page_app/views.py
+++ page_app/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def index (request):
     return render(request,"page_app/partial/home.html")
-
+def services (request):
+    return render(request, "page_app/partial/services.html")
 def contato (request):
     return render(request, "page_app/partial/contato.html")
